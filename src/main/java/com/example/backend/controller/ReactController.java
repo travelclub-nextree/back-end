4e4514d6c1e6e0fbd67fedf593f5b0a8fa9c09f2
BBeedroid <maisy13@naver.com>
2023-12-08 11:21:29 +0900

    feat: react BrowserRouter 경로 리다이랙트 설정

---
+++ src/main/java/com/example/backend/controller/ReactController.java
@@ -0,0 +1,13 @@
+package com.example.backend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ReactController {
+
+    @RequestMapping(value = "/{path:[^\\.]*}")
+    public String forwardToIndex() {
+        return "forward:/";
+    }
+}
